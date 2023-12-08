94138b41da0c70e4a97781c8262d92c569ec0003
pron212 <102412368+pron212@users.noreply.github.com>
2023-12-08 17:07:32 +0000

    Update settings.py
    Logging features added

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -46,6 +46,33 @@ CSP_IMG_SRC = ("'allow-all'",'127.0.0.1:8000', '127.0.0.1')
 CSP_STYLE_SRC = ("'allow-all'", '127.0.0.1:8000', '127.0.0.1')
 CSP_SCRIPT_SRC = ("'allow-all'", '127.0.0.1:8000', '127.0.0.1')
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        'myapp': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
 
 ROOT_URLCONF = 'student_management_system.urls'
 
